d9042285b6eb5fda418a791781dd43d69b08ba4b
Thomas Davis <thomasalwyndavis@gmail.com>
2013-10-14 09:28:35 +1000

    fist commt

---
+++ widget.js
@@ -0,0 +1,28 @@
+/*
+  Taskforce Widget
+  A generic widget include script which we will use to show further campaigns etc
+  <thomasalwyndavis@gmail.com> for support
+/*
+
+/* <script type="text/javascript">
+    window._idl = {};
+    _idl.variant = "modal";
+    _idl.campaign = "defundnsa";
+    (function() {
+        var idl = document.createElement('script');
+        idl.type = 'text/javascript';
+        idl.async = true;
+        idl.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'members.internetdefenseleague.org/include/?url=' + (_idl.url || '') + '&_idl_test=1&campaign=' + (_idl.campaign || '') + '&variant=' + (_idl.variant || 'banner');
+        document.getElementsByTagName('body')[0].appendChild(idl);
+    })();
+</script>
+
+*/
+
+
+var causes = {
+  stopwatchingus: {
+
+  }
+}
+
