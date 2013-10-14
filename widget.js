c19f72bcf26c0f2897d2c8dc9493b30afaa3c61e
Thomas Davis <thomasalwyndavis@gmail.com>
2013-10-14 13:18:19 +1000

    banner version

--- widget.js
+++ widget.js
@@ -122,7 +122,8 @@ var _tfrce_config = (typeof tfrce_config  !== 'undefined') ? tfrce_config  : {};
           campaign_container: 'position:absolute;width:100%;top:0;opacity:1;z-index:100;background: #fff',
           iframe_container: 'position: relative; height: 100px;margin: 40px 40px 0px 40px;background: #444;border-radius: 10px;',
           iframe: 'width: 100%;height: 100%;border: 0;margin:0; padding:0; border-radius: 10px;',
-          closeButton: 'position: absolute;top:0;right:0'
+          closeButton: 'border: 0;height: 28px;width: 28px;cursor: pointer;position: absolute;top:34px;right:20px; background: url("../stopwatchingus/images/close-button.png");'
+
         }
       },
       show: function () {
@@ -139,7 +140,6 @@ var _tfrce_config = (typeof tfrce_config  !== 'undefined') ? tfrce_config  : {};
         iframe_container.appendChild(iframe);
         var closeButton = document.createElement('button');
         closeButton.style.cssText = style.closeButton;
-        closeButton.innerHTML = 'close-button';
         iframe_container.appendChild(closeButton);
         closeButton.onclick = function() {
           active_campaign.hide(campaign_container)
