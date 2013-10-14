3fc8cdc4235d97b9eff380b3858f513de0e8d0c6
Thomas Davis <thomasalwyndavis@gmail.com>
2013-10-14 14:13:34 +1000

    modal example

--- widget.js
+++ widget.js
@@ -123,7 +123,12 @@ var _tfrce_config = (typeof tfrce_config  !== 'undefined') ? tfrce_config  : {};
           iframe_container: 'position: relative; height: 100px;margin: 40px 40px 0px 40px;background: #444;border-radius: 10px;',
           iframe: 'width: 100%;height: 100%;border: 0;margin:0; padding:0; border-radius: 10px;',
           closeButton: 'border: 0;height: 28px;width: 28px;cursor: pointer;position: absolute;top:33px;right:20px; background: url("../stopwatchingus/images/close-button.png");'
-
+        },
+        center_modal: {
+          campaign_container: 'position:absolute;left: 50%; margin-left: -300px; width: 600px;top:0;opacity:1;z-index:100;background: #fff',
+          iframe_container: 'position: relative; height: 500px;margin: 40px 40px 0px 40px;background: #444;border-radius: 10px;',
+          iframe: 'width: 100%;height: 100%;border: 0;margin:0; padding:0; border-radius: 10px;',
+          closeButton: 'border: 0;height: 28px;width: 28px;cursor: pointer;position: absolute;top:33px;right:20px; background: url("../stopwatchingus/images/close-button.png");'
         }
       },
       show: function () {
