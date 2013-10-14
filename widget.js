6650c6c6f2f4cf7ee06a6c4b5cc8bc8993647874
Thomas Davis <thomasalwyndavis@gmail.com>
2013-10-14 13:36:57 +1000

    some change

--- widget.js
+++ widget.js
@@ -122,7 +122,7 @@ var _tfrce_config = (typeof tfrce_config  !== 'undefined') ? tfrce_config  : {};
           campaign_container: 'position:absolute;width:100%;top:0;opacity:1;z-index:100;background: #fff',
           iframe_container: 'position: relative; height: 100px;margin: 40px 40px 0px 40px;background: #444;border-radius: 10px;',
           iframe: 'width: 100%;height: 100%;border: 0;margin:0; padding:0; border-radius: 10px;',
-          closeButton: 'border: 0;height: 28px;width: 28px;cursor: pointer;position: absolute;top:34px;right:20px; background: url("../stopwatchingus/images/close-button.png");'
+          closeButton: 'border: 0;height: 28px;width: 28px;cursor: pointer;position: absolute;top:33px;right:20px; background: url("../stopwatchingus/images/close-button.png");'
 
         }
       },
