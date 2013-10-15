aaa286b47e7948500d47ab4b619005399c65cf04
Thomas Davis <thomasalwyndavis@gmail.com>
2013-10-15 14:34:42 +1000

    close button was relative

--- widget.js
+++ widget.js
@@ -122,25 +122,25 @@ var _tfrce_config = (typeof tfrce_config  !== 'undefined') ? tfrce_config  : {};
           campaign_container: 'position:fixed;width: 100%;top:0;opacity:1;z-index:100;',
           iframe_container: 'position: relative;height: 100px;margin: 20px 20px 0px 20px;background: #444;border-radius: 10px;',
           iframe: 'width: 100%;height: 100%;border: 0;margin:0;padding:0;border-radius: 10px;',
-          closeButton: 'border: 0;height: 28px;width: 28px;cursor: pointer;position: absolute;top:33px;right:20px;background: url("../stopwatchingus/images/close-button.png");'
+          closeButton: 'border: 0;height: 28px;width: 28px;cursor: pointer;position: absolute;top:33px;right:20px;background: url("' + ASSET_URL +'images/close-button.png");'
         },
         bottom_large_banner: {
           campaign_container: 'position:fixed;width:100%;bottom:0;opacity:1;z-index:100;background: #fff',
           iframe_container: 'position: relative;height: 100px;margin: 0px 20px 20px 20px;background: #444;border-radius: 10px;',
           iframe: 'width: 100%;height: 100%;border: 0;margin:0;padding:0;border-radius: 10px;',
-          closeButton: 'border: 0;height: 28px;width: 28px;cursor: pointer;position: absolute;top:33px;right:20px;background: url("../stopwatchingus/images/close-button.png");'
+          closeButton: 'border: 0;height: 28px;width: 28px;cursor: pointer;position: absolute;top:33px;right:20px;background: url("' + ASSET_URL +'images/close-button.png");'
         },
         top_small_banner: {
           campaign_container: 'position:fixed;width:100%;top:0;opacity:1;z-index:100;background: #fff',
           iframe_container: 'position: relative;height: 100px;margin: 20px 20px 0px 20px;background: #444;border-radius: 10px;',
           iframe: 'width: 100%;height: 100%;border: 0;margin:0;padding:0;border-radius: 10px;',
-          closeButton: 'border: 0;height: 28px;width: 28px;cursor: pointer;position: absolute;top:33px;right:20px;background: url("../stopwatchingus/images/close-button.png");'
+          closeButton: 'border: 0;height: 28px;width: 28px;cursor: pointer;position: absolute;top:33px;right:20px;background: url("' + ASSET_URL +'images/close-button.png");'
         },
         center_modal: {
           campaign_container: 'position:fixed;left: 50%;margin-left: -300px;margin-top: 40px;width: 600px;top:0;opacity:1;z-index:100;background: #fff',
           iframe_container: 'position: relative;height: 325px; width: 600px;background: #444;border-radius: 10px;',
           iframe: 'width: 100%;height: 100%;border: 0;margin:0;padding:0;border-radius: 10px;',
-          closeButton: 'border: 0;height: 28px;width: 28px;cursor: pointer;position: absolute;top:15px;right:15px;background: url("../stopwatchingus/images/close-button.png");'
+          closeButton: 'border: 0;height: 28px;width: 28px;cursor: pointer;position: absolute;top:15px;right:15px;background: url("' + ASSET_URL +'images/close-button.png");'
         }
       },
       show: function () {
