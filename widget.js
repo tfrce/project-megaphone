d43be69f466936d039fb2253bb9a04761987753a
Sina Khanifar <sina.khanifar@gmail.com>
2013-10-14 23:39:07 -0700

    Some modal CSS changes, added links

--- widget.js
+++ widget.js
@@ -143,9 +143,9 @@ var _tfrce_config = (typeof tfrce_config  !== 'undefined') ? tfrce_config  : {};
           closeButton: 'border:0;height:28px;width:28px;cursor:pointer;position:absolute;top:11px;right:10px;background:url("' + ASSET_URL +'images/close-button.png");'
         },
         center_modal: {
-          overlay: 'position:absolute; background:#000; width:100%; height:100%;z-index:30;top:0; left:0; opacity:0.5',
+          overlay: 'position:fixed; background:#000; width:100%; height:100%;z-index:30;top:0; left:0; opacity:0.5',
           campaign_container: 'position:fixed;left:50%;margin-left:-250px;margin-top:40px;width:500px;top:0;opacity:1;z-index:100;',
-          iframe_container: 'position:relative;height:325px; width:500px;background:#444;border-radius:10px;',
+          iframe_container: 'position:relative;height:365px; width:500px;background:#444;border-radius:10px;',
           iframe: 'width:100%;height:100%;border:0;margin:0;padding:0;border-radius:10px;',
           closeButton: 'border:0;height:28px;width:28px;cursor:pointer;position:absolute;top:15px;right:15px;background:url("' + ASSET_URL +'images/close-button.png");'
         }
