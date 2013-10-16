8c01a724c449c3cfb431e4beb48d21df6766f3e5
Thomas Davis <thomasalwyndavis@gmail.com>
2013-10-16 15:12:59 +1000

    opacity fix

--- widget.js
+++ widget.js
@@ -108,7 +108,7 @@ var _tfrce_config = (typeof tfrce_config  !== 'undefined') ? tfrce_config  : {};
       startDate: new Date(2011, 10, 30, 0),
       endDate: new Date(2014, 10, 30, 0),
       hide: function (el, callback) {
-        el.remove();
+        document.body.removeChild(el);
         setCookie(active_campaign.cookieName, 'true', COOKIE_TIMEOUT);
         if(callback) { callback(); };
       },
@@ -138,7 +138,7 @@ var _tfrce_config = (typeof tfrce_config  !== 'undefined') ? tfrce_config  : {};
           closeButton: 'border:0;height:28px;width:28px;cursor:pointer;position:absolute;top:11px;right:10px;background:url("' + ASSET_URL +'images/close-button.png");'
         },
         modal: {
-          overlay: 'position:fixed; background:#000; width:100%; height:100%;z-index:30;top:0; left:0; opacity:0.5',
+          overlay: 'position:fixed; background:#000; width:100%; height:100%;z-index:30;top:0; left:0; opacity:0.5;-moz-opacity: 0.50;filter: alpha(opacity=50);',
           campaign_container: 'position:fixed;left:50%;margin-left:-250px;margin-top:40px;width:500px;top:0;opacity:1;z-index:100;',
           iframe_container: 'position:relative;height:365px; width:500px;background:#444;border-radius:10px;',
           iframe: 'width:100%;height:100%;border:0;margin:0;padding:0;border-radius:10px;',
@@ -185,7 +185,7 @@ var _tfrce_config = (typeof tfrce_config  !== 'undefined') ? tfrce_config  : {};
         closeButton.onclick = function() {
           active_campaign.hide(campaign_container);
           if(style.overlay) {
-            overlay.remove();
+            document.body.removeChild(overlay);
           }
         }
 
