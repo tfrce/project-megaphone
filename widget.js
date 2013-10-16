f2d81e4c88953807c6645b544c2cc90251957bbc
Sina Khanifar <sina.khanifar@gmail.com>
2013-10-16 01:20:09 -0700

    pushed z-index and -webkit/moz-box-sizing fixes

--- widget.js
+++ widget.js
@@ -112,26 +112,26 @@ var _tfrce_config = (typeof tfrce_config  !== 'undefined') ? tfrce_config  : {};
       },
       styles: {
         banner: {
-          campaign_container: 'position:fixed;width:100%;bottom:0;z-index:100; padding: 0 20px;',
+          campaign_container: 'position:fixed;width:100%;bottom:0;z-index:100000; padding: 0 20px;-webkit-box-sizing: border-box; -moz-box-sizing: border-box;',
           iframe_container: 'position: relative; height: 100px; max-width:1201px; margin: 0px auto 8px auto; background: #444;border-radius: 10px;',
           iframe: 'width: 100%;height: 100%;border: 0;margin:0;padding:0;border-radius: 10px;',
           closeButton: 'border: 0;height: 28px;width: 28px;cursor: pointer;position: absolute;top:33px;right:20px;background: url("' + ASSET_URL +'images/close-button.png");'
         },
         dev: {
-          campaign_container: 'position:fixed;width:100%;bottom:0;z-index:100; padding: 0 20px;',
+          campaign_container: 'position:fixed;width:100%;bottom:0;z-index:100000; padding: 0 20px;-webkit-box-sizing: border-box; -moz-box-sizing: border-box;',
           iframe_container: 'position: relative; height: 100px; max-width:1201px; margin: 0px auto 8px auto; background: #444;border-radius: 10px;',
           iframe: 'width: 100%;height: 100%;border: 0;margin:0;padding:0;border-radius: 10px;',
           closeButton: 'border: 0;height: 28px;width: 28px;cursor: pointer;position: absolute;top:33px;right:20px;background: url("' + ASSET_URL +'images/close-button.png");'
         },
         strip: {
-          campaign_container: 'position:fixed;width:100%;bottom:0;opacity:1;z-index:100;',
+          campaign_container: 'position:fixed;width:100%;bottom:0;opacity:1;z-index:100000;-webkit-box-sizing: border-box; -moz-box-sizing: border-box;',
           iframe_container: 'position:relative;height:50px;margin:0 20px 0px 20px;background:#444;border-radius:10px 10px 0 0;',
           iframe: 'width:100%;height:100%;border:0;margin:0;padding:0;border-radius:10px;',
           closeButton: 'border:0;height:28px;width:28px;cursor:pointer;position:absolute;top:11px;right:10px;background:url("' + ASSET_URL +'images/close-button.png");'
         },
         modal: {
-          overlay: 'position:fixed; background:#000; width:100%; height:100%;z-index:30;top:0; left:0; opacity:0.5;-moz-opacity: 0.50;filter: alpha(opacity=50);',
-          campaign_container: 'position:fixed;left:50%;margin-left:-250px;margin-top:40px;width:500px;top:0;opacity:1;z-index:100;',
+          overlay: 'position:fixed; background:#000; width:100%; height:100%;z-index:100000;top:0; left:0; opacity:0.5;-moz-opacity: 0.50;filter: alpha(opacity=50);',
+          campaign_container: 'position:fixed;left:50%;margin-left:-250px;margin-top:40px;width:500px;top:0;opacity:1;z-index:100010;',
           iframe_container: 'position:relative;height:365px; width:500px;background:#444;border-radius:10px;',
           iframe: 'width:100%;height:100%;border:0;margin:0;padding:0;border-radius:10px;',
           closeButton: 'border:0;height:28px;width:28px;cursor:pointer;position:absolute;top:15px;right:15px;background:url("' + ASSET_URL +'images/close-button.png");'
