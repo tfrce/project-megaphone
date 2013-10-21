5b4d60ad810dcacc8661f7f97a2c1378a6690d10
hellekin <hellekin@cepheide.org>
2013-10-21 12:00:13 -0300

    Add AGPLv3+ license to HTML and JavaScript files (refs #7)

--- widget.js
+++ widget.js
@@ -13,6 +13,30 @@
 
   ===============================================================================
 
+  @source: https://raw.github.com/tfrce/project-megaphone/gh-pages/widget.js
+
+  @licstart  The following is the entire license notice for the
+             JavaScript code in this page.
+
+  Copyright (C) 2013 Taskforce <http://taskforce.is>
+
+  The JavaScript code in this page is free software: you can
+  redistribute it and/or modify it under the terms of the GNU Affero
+  General Public License (GNU GPL) as published by the Free Software
+  Foundation, either version 3 of the License, or (at your option) any
+  later version.  The code is distributed WITHOUT ANY WARRANTY;
+  without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+  A PARTICULAR PURPOSE.  See the GNU GPL for more details.
+ 
+  As additional permission under GNU AGPL version 3 section 7, you may
+  distribute non-source (e.g., minimized or compacted) forms of that
+  code without the copy of the GNU AGPL normally required by section
+  4, provided you include this license notice and a URL through which
+  recipients can access the Corresponding Source.
+ 
+ @licend  The above is the entire license notice for the JavaScript
+          code in this page.
+
 */
 
 // Wrap widget in function to protect scope
